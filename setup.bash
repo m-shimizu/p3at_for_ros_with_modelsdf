fa0c5369082a857bdeb072a1a266a97f3464cbec
Masaru Shimizu <shimizu@sist.chukyo-u.ac.jp>
2017-03-07 14:42:29 +0900

    Fix setup.bash

--- setup.bash
+++ setup.bash
@@ -1,3 +1,3 @@
 source devel/setup.bash
-export GAZEBO_MODEL_PATH=${HOME}/p3at_for_ros_with_modelsdf/models:${GAZEBO_MODEL_PATH}
-export GAZEBO_PLUGIN_PATH=/opt/ros/indigo/lib:${GAZEBO_PLUGIN_PATH}
+export GAZEBO_MODEL_PATH=`pwd`/models:${GAZEBO_MODEL_PATH}
+export GAZEBO_PLUGIN_PATH=`pwd`/devel/lib:/opt/ros/indigo/lib:${GAZEBO_PLUGIN_PATH}
