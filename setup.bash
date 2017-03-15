93f4b9a17e9f2b2e44d3351f155d6ae691e6f61f
Masaru Shimizu <shimizu@sist.chukyo-u.ac.jp>
2017-03-15 19:00:06 +0900

    Add thermal camera on P2DX, add two models: box_hot, box_red and which_is_hot.world

--- setup.bash
+++ setup.bash
@@ -1,3 +1,4 @@
 source devel/setup.bash
 export GAZEBO_MODEL_PATH=`pwd`/models:${GAZEBO_MODEL_PATH}
-export GAZEBO_PLUGIN_PATH=`pwd`/devel/lib:/opt/ros/indigo/lib:${GAZEBO_PLUGIN_PATH}
+#export GAZEBO_PLUGIN_PATH=`pwd`/devel/lib:/opt/ros/kinetic/lib:${HOME}/hector_gazebo/devel/lib:${GAZEBO_PLUGIN_PATH}
+export GAZEBO_PLUGIN_PATH=`pwd`/devel/lib:/opt/ros/kinetic/lib:${GAZEBO_PLUGIN_PATH}
